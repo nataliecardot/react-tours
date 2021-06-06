25151d506a068265d6a0ed72170b4b0d9b521f04
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-06 10:52:23 -0700

    Fetch tours

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ const url = 'https://course-api.com/react-tours-project';
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json(); // Parse HTTP response as JSON object
+  };
+
+  // Invoke fetchTours once app component has rendered
+  // Passing [] (empty dependency array) as second arg ensures it's only called once, after initial render - by default useEffect is called after first render and after every rerender
+  useEffect(() => fetchTours(), []);
+
   if (loading) {
     return (
       <main>
