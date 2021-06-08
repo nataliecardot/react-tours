db2b9b30ab265936933ec5a6f3e27846e69c40de
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-07 20:56:16 -0700

    Add show more/less buttons

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +10,14 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">{price}</h4>
         </div>
-        <p>{info}</p>
+        {/* substring: characters including spaces and punctuation */}
+        <p>
+          {readMore ? info : `${info.substring(0, 200).trim()}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'Show less...' : 'Read more...'}
+          </button>
+        </p>
+        <button className="delete-btn">Not Interested</button>
       </footer>
     </article>
   );
