c0fedb3fb11791a68be86fe06974d8e98f6df207
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-10 22:14:14 -0700

    Conditionally set message/button to refresh tours

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
 import Tours from './Tours';
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
@@ -14,25 +12,25 @@ function App() {
     setTours(newTours);
   };
 
+  const fetchTours = async () => {
+    setLoading(true);
+
+    // Catch network errors
+    try {
+      const response = await fetch(url);
+      // Despite the method being named json(), the result is not JSON but is instead the result of taking JSON as input and parsing it to produce a JavaScript object https://developer.mozilla.org/en-US/docs/Web/API/Body/json
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
   // Invoke fetchTours once app component has rendered
   // Passing [] (empty dependency array) as second arg ensures it's only called once, after initial render - by default useEffect is called after first render and after every rerender
   useEffect(() => {
-    const fetchTours = async () => {
-      setLoading(true);
-
-      // Catch network errors
-      try {
-        const response = await fetch(url);
-        // Despite the method being named json(), the result is not JSON but is instead the result of taking JSON as input and parsing it to produce a JavaScript object https://developer.mozilla.org/en-US/docs/Web/API/Body/json
-        const tours = await response.json();
-        setLoading(false);
-        setTours(tours);
-      } catch (error) {
-        setLoading(false);
-        console.log(error);
-      }
-    };
-
     fetchTours();
   }, []);
 
@@ -40,6 +38,18 @@ function App() {
     return (
       <main>
         <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
       </main>
     );
   }
